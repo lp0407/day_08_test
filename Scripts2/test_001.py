899ab3087da5dbd1c88da71f3cc07b4d79501e27
lp0407 <16619710374@163.com>
2018-07-06 19:18:28 +0800

    update

--- Scripts2/test_001.py
+++ Scripts2/test_001.py
@@ -1,10 +1,8 @@
 import allure
 class Test_001:
-    @allure.step(title="第一个测试")
     def test_001(self):
         allure.attach("这是一个描述","我是具体内容")
         assert 0
 
-    @allure.step(title="测试步骤002")
     def test_002(self):
         assert 1
